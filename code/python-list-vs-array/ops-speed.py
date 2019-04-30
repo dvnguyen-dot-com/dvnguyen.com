5cb6f691f50c70b83d31432713897af6c518b2b4
Dave Nguyen <dave@hvst.com>
2019-04-30 13:56:26 -0500

    I'm sorry, I don't remember what I was doing

--- code/python-list-vs-array/ops-speed.py
+++ code/python-list-vs-array/ops-speed.py
@@ -3,4 +3,16 @@
     get, index, insert, repleated concatenation, im-place repeated concatenation,
     reversed repeated concatenation, pop, remove, reverse."""
 
+import random, array
+
+# append
+li = []
+arr = array.array('d', [])
+
+for i in range(10000):
+    li.append(random.random())
+
+for i in range(10000):
+    arr.append(random.random())
+
 
